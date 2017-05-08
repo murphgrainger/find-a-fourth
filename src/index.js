0b21441ad25cf4cf32ac35cfae25f636cc47f7a1
Murph Grainger <murph.grainger@gmail.com>
2017-05-08 15:48:41 -0600

    fixed typo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>
   document.getElementById('root')
 );
