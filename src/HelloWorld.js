d822d6630f1777776e0a5f564a8441b0927699bd
Murph Grainger <murph.grainger@gmail.com>
2017-05-08 11:02:20 -0600

    initial commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component {
+
+  constructor(props) {
+  super(props);
+  this.state = { greeting: 'Hello' };
+}
+  render() {
+    return (
+      <div className="HelloWorld">
+      {this.props.greeting} {this.props.name}!</div>
+    );
+  }
+}
+
+export default HelloWorld;
