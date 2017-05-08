cf3fa01587f3f965b6ae39ca95b2c9a719caaf12
Murph Grainger <murph.grainger@gmail.com>
2017-05-08 15:05:23 -0600

    added tests

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -6,13 +6,31 @@ class HelloWorld extends Component {
   constructor(props) {
   super(props);
   this.state = { greeting: 'Hello' };
+  this.frenchify = this.frenchify.bind(this);
+  this.removeGreeting = this.removeGreeting.bind(this);
+
 }
+
+removeGreeting() {
+  this.props.removeGreeting(this.props.name);
+}
+
+frenchify() {
+this.setState({ greeting: 'Bonjour' });
+}
+
   render() {
     return (
       <div className="HelloWorld">
-      {this.props.greeting} {this.props.name}!</div>
+      {this.state.greeting} {this.props.name}!
+      <br/>
+      <button onClick={this.frenchify}>Frenchify!</button>
+      <button onClick={this.removeGreeting}>Remove Me!</button>
+      </div>
     );
   }
 }
+
+
 
 export default HelloWorld;
