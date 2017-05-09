c83d29b44a2cec80a94eb49bbcf70884a98926c3
Murph Grainger <murph.grainger@gmail.com>
2017-05-09 16:44:50 -0600

    get post route initialized

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -3,35 +3,51 @@ import {Button} from 'react-bootstrap';
 
 import './search.css'
 
-class SearchPage extends React.Component {
+const LOCAL_URL= 'http://localhost:4000'
 
+
+class SearchPage extends React.Component {
   constructor(props) {
   super(props);
   this.state = {};
-  this.getCourse = this.getCourse.bind(this);
+  this.getPosts = this.getPosts.bind(this);
 }
 
-
-
-getCourse() {
-  let url = 'http://localhost:4000/swingAPI'
-  fetch(url, {
-	method: 'get',
-  mode:'cors'
-}).then(response => {
-return response.json()
-  }).then(data => {
-  console.log(data)
-})
-}
     render() {
         return (
           <div className="container">
-            <h1>Search for Tee Times</h1>
-            <Button bsStyle="primary" onClick={this.getCourse}>Get Course</Button>
+            <h1>Join a Group</h1>
+            <Button bsStyle="primary" onClick={this.getPosts}>Get Posts</Button>
           </div>
         );
     }
+
+    getCourses() {
+      let url = `${LOCAL_URL}/swingAPI`
+      fetch(url, {
+    	method: 'get',
+      mode:'cors'
+    }).then(response => {
+    return response.json()
+      }).then(data => {
+      console.log(data)
+      })
+    }
+
+    getPosts() {
+      let url = `${LOCAL_URL}/post`;
+      fetch(url, {
+        method: 'get',
+        mode: 'cors'
+      }).then(res => {
+        return res.json()
+      }).then(data => {
+        console.log(data);
+      })
+    }
+
+
+
 }
 
 export default SearchPage;
