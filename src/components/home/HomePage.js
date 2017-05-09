8b2c61da1a116606a3b69f9f63f6a3cbbc3db5af
Murph Grainger <murph.grainger@gmail.com>
2017-05-09 10:55:51 -0600

    initial homepage edit

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, Button, Thumbnail } from 'react-bootstrap';
 
 import './home.css'
 
 class HomePage extends React.Component {
     render() {
         return (
-          <div className>
-            <Grid fluid>
-              <Row>
-                <Col xs={12} s={6} md={6}>
-                </Col>
-                <Col xs={12} s={6}md={6}>
-                  <div>
-                    <h4 className="headline">Golf with a compatible group</h4>
-                    <ul className="list-items">
+          <div className = "container">
+              <Jumbotron>
+                <div className="container">
+                    <h1 className="headline">Compatible Groups</h1>
+                    <p>Search or post tee times to form groups based on your preferences.</p>
+               </div>
+              </Jumbotron>
+              <Grid>
+                <Row>
+                <Col xs={6} md={6}>
+                  <Thumbnail>
+                    <h3>Search</h3>
+                    <p>Find an existing group via multiple methods:</p>
+                    <ul>
+                      <li>Time Span</li>
+                      <li>Course or Region</li>
                       <li>Handicap</li>
-                      <li> Age</li>
-                      <li> Gender</li>
-                      <li> Course</li>
+                      <li>Age or Gender</li>
                     </ul>
-                </div>
+                    <p>
+                      <Button bsStyle="primary">Search</Button>
+                    </p>
+                  </Thumbnail>
                 </Col>
-              </Row>
+                <Col xs={6} md={6}>
+                  <Thumbnail>
+                    <h3>Post</h3>
+                    <p>Post an existing tee time with pre-selected preferences for the remaining golfers who will join the group:</p>
+                      <ul>
+                        <li>Preferences sync with golfer profiles and include handicap, age, and gender</li>
+                        <li>Accept or reject join requests</li>
+                        <li>Contact accepted group member to confirm addition to course tee time</li>
+                      </ul>
+                    <p>
+                      <Button bsStyle="primary">Button</Button>&nbsp;
+                      <Button bsStyle="default">Button</Button>
+                    </p>
+                  </Thumbnail>
+                </Col>
+                </Row>
             </Grid>
           </div>
         );
