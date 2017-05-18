b6b347275733cb990bb059d87034981af98e4e5e
Murph Grainger <murph.grainger@gmail.com>
2017-05-18 15:56:01 -0600

    posts sort by date

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -50,8 +50,17 @@ componentDidMount() {
       }).then(res => {
         return res.json()
       }).then(data => {
+        data.sort(function (left, right) {
+        return moment.utc(left.date).diff(moment.utc(right.date))
+      });
+      data.forEach(e => {
+        console.log(e.date);
+        let str = e.date;
+        let date = moment(str);
+        e.date = date.utc().format('ddd, MMM Do');
+        console.log(e.date);
+      })
         this.setState({ posts: [...this.state.posts, ...data] });
-        console.log(this.state.posts);
       })
     }
 
