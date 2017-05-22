cdf6ff919ba7b35e8c7d6b1c31e0edbe55922321
Murph Grainger <murph.grainger@gmail.com>
2017-05-21 21:31:26 -0600

    more cleaning

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -25,29 +25,30 @@ class HomePage extends React.Component {
                   <Col xs="12" md="8">
                     <h2>Search</h2>
                       <h6>Find an existing group based on search preferences.</h6>
-                      <p><Icon name='filter' size='2x'/>&nbsp; Handicap</p>
-                      <p> <Icon name='filter' size='2x'/>&nbsp; Age</p>
-                      <p><Icon name='filter' size='2x'/>&nbsp; Gender</p>
+                      <p><Icon name='filter' size='1x'/>&nbsp; Handicap</p>
+                      <p> <Icon name='filter' size='1x'/>&nbsp; Age</p>
+                      <p><Icon name='filter' size='1x'/>&nbsp; Gender</p>
                       <p>
-                        <Button bsStyle="primary" href="/search">Search</Button>
+                        <Button color="primary" href="/search">Search</Button>
                       </p>
                   </Col>
                   <Col xs="12" md="4">
                     <img src='https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=250'/>
                   </Col>
                   </Row>
-                  <Row style={{width: '100%'}}>
+                  <hr className="section-break"/>
+                  <Row style={{width: '100%', padding:'1rem'}}>
                     <Col xs="12" md="4">
                       <img src='https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=250'/>
                   </Col>
                   <Col xs="12" md="8">
                     <h2>Post</h2>
                     <h6>Post an existing group with pre-selected preferences for the remaining golfers who will join.</h6>
-                    <p><Icon name='refresh' size='2x'/>&nbsp; Preferences sync with golfer profiles and include handicap, age, and gender</p>
-                    <p><Icon name='check' size='2x'/>&nbsp; Accept or reject join requests</p>
-                    <p><Icon name='user' size='2x'/>&nbsp; Contact accepted group member to confirm addition to course tee time</p>
+                    <p><Icon name='refresh' size='1x'/>&nbsp; Preferences sync with golfer profiles and include handicap, age, and gender</p>
+                    <p><Icon name='check' size='1x'/>&nbsp; Accept or reject join requests</p>
+                    <p><Icon name='user' size='1x'/>&nbsp; Contact accepted group member to confirm addition to course tee time</p>
                   <p>
-                    <Button bsStyle="primary" href="/post">Post</Button>
+                    <Button color="primary" href="/post">Post</Button>
                   </p>
                   </Col>
                 </Row>
