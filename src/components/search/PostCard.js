72bb14caf36cc2204df0f085ab3d7b5da4183202
Murph Grainger <murph.grainger@gmail.com>
2017-05-23 11:27:48 -0600

    moved users to corner

--- src/components/search/PostCard.js
+++ src/components/search/PostCard.js
@@ -13,12 +13,21 @@ class PostCard extends Component {
   render() {
     return (
       <Card inverse className="post-card">
-        <CardHeader className="date" inverse style={{ backgroundColor: '#475B5A', borderColor: '#475B5A' }}><Icon name='calendar' size='2x'/> &nbsp;{this.props.post.date}</CardHeader>
+        <CardHeader className="date" inverse style={{ backgroundColor: '#475B5A', borderColor: '#475B5A' }}>
+          <Row>
+          <Col xs="10" lg="10" className="date-col">
+          <Icon name='calendar' size='1x'/> &nbsp; {this.props.post.date}
+          </Col>
+          <Col xs="2" lg="2" className="user-col">
+          <Icon name='user' size='1x'/> <small>{this.props.post.group_count}</small>
+          </Col>
+          </Row>
+          </CardHeader>
           <CardBlock>
           <ListGroup>
+          <ListGroupItem className="preferences"> <Icon name='map-marker' size='1x'/> &nbsp; Colorado Golf Club</ListGroupItem>
           <ListGroupItem className="preferences">{this.props.post.handicap_min} to {this.props.post.handicap_max} Index</ListGroupItem>
           <ListGroupItem className="preferences">{this.props.post.age_min} to {this.props.post.age_max} yrs</ListGroupItem>
-          <ListGroupItem className="preferences">{this.props.post.group_count} Player(s)</ListGroupItem>
           </ListGroup>
         </CardBlock>
         <CardFooter><Button block className="color-hit-orange">Join</Button></CardFooter>
