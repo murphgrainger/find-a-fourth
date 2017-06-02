5926b7011af9392e10131fcf3216906af5db6498
Murph Grainger <murph.grainger@gmail.com>
2017-06-02 11:12:26 -0600

    formatted card columns

--- src/components/search/PostCard.js
+++ src/components/search/PostCard.js
@@ -15,14 +15,14 @@ class PostCard extends Component {
       <Card className="course-card">
         <CardHeader>  <Icon name='calendar'/> &nbsp; {this.props.post.date}</CardHeader>
         <CardBlock className="card-row">
-        <Col xs="12" md="4" className="location-col">
+        <Col xs="12" md="3" className="location-col">
           <p><Icon name='map-marker'/> &nbsp; {this.props.post.address}</p>
         </Col>
         <Col xs="12" md="3" className="centered-col">
           <small>Age Range</small>
           <p>{this.props.post.age_min} - {this.props.post.age_max}</p>
         </Col>
-        <Col xs="12" md="3" className="centered-col">
+        <Col xs="12" md="2" className="centered-col">
           <small>Handicap Range</small>
           <p>{this.props.post.handicap_min} - {this.props.post.handicap_max}</p>
         </Col>
@@ -30,31 +30,13 @@ class PostCard extends Component {
           <small>Group Count</small>
           <p>{this.props.post.group_count}</p>
         </Col>
+        <Col xs="12" md="2" className="centered-col">
+          <Button block className="color-hit-orange">Join</Button>
+        </Col>
         </CardBlock>
       </Card>
     );
   }
 }
-
-// <Card inverse className="post-card">
-//   <CardHeader className="date" style={{ backgroundColor: '#475B5A', borderColor: '#475B5A' }}>
-//     <Row>
-//     <Col xs="10" lg="10" className="date-col">
-//     <Icon name='calendar'/> &nbsp; {this.props.post.date}
-//     </Col>
-//     <Col xs="2" lg="2" className="user-col">
-//     <Icon name='user'/> <small>{this.props.post.group_count}</small>
-//     </Col>
-//     </Row>
-//     </CardHeader>
-//     <CardBlock>
-//     <ListGroup>
-//     <ListGroupItem className="preferences"> <Icon name='map-marker'/> &nbsp; {this.props.post.address}</ListGroupItem>
-//     <ListGroupItem className="preferences">{this.props.post.handicap_min} to {this.props.post.handicap_max} Index</ListGroupItem>
-//     <ListGroupItem className="preferences">{this.props.post.age_min} to {this.props.post.age_max} yrs</ListGroupItem>
-//     </ListGroup>
-//   </CardBlock>
-//   <CardFooter><Button block className="color-hit-orange">Join</Button></CardFooter>
-// </Card>
 
 export default PostCard;
