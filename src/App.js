5f163297ae3a54afcdfcb1b319a7965d8eb37327
Murph Grainger <murph.grainger@gmail.com>
2017-06-07 16:55:47 -0600

    router redo with auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from './components/common/Header.js'
+import Home from './components/home/HomePage.js'
+
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 };
