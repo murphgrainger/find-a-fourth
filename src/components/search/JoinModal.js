03bf0f47c52132826238496887c5a09ff725f1fc
Murph Grainger <murph.grainger@gmail.com>
2017-07-31 16:54:30 -0600

    reworked col in modal

--- src/components/search/JoinModal.js
+++ src/components/search/JoinModal.js
@@ -15,8 +15,7 @@ class JoinModal extends React.Component {
   }
 
   toggle() {
-    console.log(this.props.auth);
-    if (this.props.auth.isAuthenticated()) {
+   if (this.props.auth.isAuthenticated()) {
       this.setState({
         modal: !this.state.modal
       });
@@ -32,14 +31,15 @@ class JoinModal extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Confirm Join Request</ModalHeader>
           <ModalBody>
-            <Row>
-            <Col sm='12' md='6'>
             <Card className="preview-card">
-              <CardHeader>Post</CardHeader>
+              <CardHeader>{this.props.post.date}</CardHeader>
               <CardBlock className="card-column">
-                <p><Icon name='calendar'/> &nbsp; {this.props.post.date} </p>
                 <p><Icon name='map-marker'/> &nbsp; {this.props.post.address}</p>
                 <Row>
+                <Col sm='12' md='7'>
+                  <img src='http://via.placeholder.com/250x150'/>
+                </Col>
+                <Col sm='12' md='5'>
                   <Col sm="12" md="6" className="preview-card-detail-col">
                 <small>Age Range</small>
                 <p>{this.props.post.age_min} - {this.props.post.age_max}</p>
@@ -48,30 +48,10 @@ class JoinModal extends React.Component {
                 <small>Handicap Range</small>
                 <p>{this.props.post.handicap_min} - {this.props.post.handicap_max}</p>
                 </Col>
-              </Row>
+                </Col>
+                </Row>
               </CardBlock>
             </Card>
-          </Col>
-            <Col sm='12' md='6'>
-              <Card className="preview-card">
-                <CardHeader> Profile </CardHeader>
-                <CardBlock className="card-column">
-                  <p><Icon name='user'/> &nbsp; Johnny Jones</p>
-                  <p><Icon name='map-marker'/> &nbsp; Sunnyside, CO</p>
-                  <Row>
-                  <Col sm="12" md="6" className="preview-card-detail-col">
-                    <small>Age</small>
-                    <p>25</p>
-                  </Col>
-                  <Col sm="12" md="6" className="preview-card-detail-col">
-                    <small>Handicap</small>
-                    <p>8</p>
-                  </Col>
-                </Row>
-                </CardBlock>
-              </Card>
-            </Col>
-            </Row>
           </ModalBody>
           <ModalFooter>
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
